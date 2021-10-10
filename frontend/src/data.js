5638e2f89c17fd7afccaa49a91a5cd4866927b92
Anthony-Ricse <dokouser98@gmail.com>
2021-10-10 14:28:41 -0500

    part5End

---
+++ frontend/src/data.js
@@ -0,0 +1,33 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name : 'Platano de seda',
+            category:'fruta',
+            image:'/images/platano.jpg',
+            price : 4.5,
+            numReviews : 10 , 
+            description:'platano de seda sin gluten' ,
+        },
+        {
+            _id:'2',
+            name : 'Platano de seda',
+            category:'fruta',
+            image:'/images/lenteja.jpg',
+            price : 4.5,
+            numReviews : 10 , 
+            description:'platano de seda sin gluten' ,
+        },
+        {
+            _id:'3',
+            name : 'Lenteja Paisana',
+            category:'Legumbre',
+            image:'/images/platano.jpg',
+            price : 4.5,
+            numReviews : 10 , 
+            description:'platano de seda sin gluten' ,
+        },
+
+    ],
+};
+export default data;
